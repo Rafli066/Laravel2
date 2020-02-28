8d21f87672576c532a7622879d6f8c2eba43c3e5
Rafli066 <akunabika3@gmail.com>
2020-02-28 16:11:56 +0700

    CRUD RELASI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,43 @@
 @extends('layouts.app')
-
 @section('content')
+
+<br>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header"><b>HALAMAN UTAMA</b></div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                        Wilujeung Sumping ! <br>
-                        Mangga Logi-in Heula.
+                    @php $no = 1; @endphp
+                    <li class="nav-item">
+                        <a href="{{route('dosen.index')}}">
+                            Dosen
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('hobi.index')}}">
+                            Hobi
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('mahasiswa.index')}}">
+                            Mahasiswa
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('wali.index')}}">
+                            Wali
+                        </a>
+                    </li>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
